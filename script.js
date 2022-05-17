731e77cfa3684593b312a5b8e298962d0b06c2c5
nicha.satt <nicha.satt@gmail.com>
2022-05-17 15:03:29 +0700

    check prime

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const n = 9;
+let n=1;
+let isPrime = true;
 
-let n1For = 0, n2For = 1, nextTermFor;
-for (let i = 0; i < n; i++) {
-    nextTermFor = n1For + n2For;
-    n1For = n2For;
-    n2For = nextTermFor;
+for(let i=2; i<n; i++) {
+    if(n%i == 0){
+        isPrime = false;
+        break;
+    }
 }
 
-let n1While = 0, n2While = 1, nextTermWhile;
-let i=0;
-while (i < n) {
-    nextTermWhile = n1While + n2While;
-    n1While = n2While;
-    n2While = nextTermWhile;
-    i++;
+if(isPrime) {
+    console.log(n +' is a prime number')
+} else {
+    console.log(n +' is not a prime number')
 }
-
-console.log('For:   Fibonacci(' +n +') = ' + n1For);
-console.log('While: Fibonacci(' +n +') = ' + n1While);
