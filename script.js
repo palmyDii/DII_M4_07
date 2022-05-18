47d7835baacd4a37fa9997adcf82c797e3493302
nicha.satt <nicha.satt@gmail.com>
2022-05-18 12:09:04 +0700

    initial commit : change text by id

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-let score = 55;
-let grade;
-if (score >= 80) {
-    grade = 'A';
-} else if (score >= 75) {
-    grade = 'B+';
-} else if (score >= 70) {
-    grade = 'B';
-} else if (score >= 65) {
-    grade = 'C+';
-} else if (score >= 60) {
-    grade = 'C';
-} else if (score >= 55) {
-    grade = 'D+';
-} else if (score >= 50) {
-    grade = 'D';
-} else {
-    grade = 'F'
-}
+const newText = 'not ok';
+document.getElementById('ok').innerHTML = newText;
 
-console.log(grade + ' ' + score)
-
-
+const newText2 = 'ok';
+document.getElementById('cancel').innerHTML = newText2;
