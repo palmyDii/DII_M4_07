f9924cf52b43d0345086fe9e471e58c12045db21
nicha.satt <nicha.satt@gmail.com>
2022-05-18 11:27:52 +0700

    employee with no function

--- script.js
+++ script.js
@@ -1,18 +1,69 @@
-let n=15;
-//display all prime number from 2-n
+let employee = [];
 
-console.log(`The prime numbers between 0 and ${n} are:`);
-
-for (let i = 0; i <= n; i++) {
-    let isPrime = 1;
-    for (let j = 2; j*j < i; j++) {
-        if (i % j == 0) {
-            isPrime = 0;
-            break;
-        }
-    }
-
-    if (i > 1 && isPrime == 1) {
-        console.log(i);
-    }
+employee[0] = {
+    'name' : 'ก',
+    'lastname' : 'นามสมมติ',
+    'age' : 68,
+    'salary' : 75900
 }
+employee[1] = {
+    'name' : 'ข',
+    'lastname' : 'นามสมมติ',
+    'age' : 35,
+    'salary' : 55400
+}
+
+employee[2] = {
+    'name' : 'ค',
+    'lastname' : 'นามสมมติ',
+    'age' : 51,
+    'salary' : 81000
+}
+
+employee[3] = {
+    'name' : 'ง',
+    'lastname' : 'นามสมมติ',
+    'age' : 76,
+    'salary' : 111000
+}
+
+employee[4] = {
+    'name' : 'ง',
+    'lastname' : 'นามสมมติ',
+    'age' : 22,
+    'salary' : 20500
+}
+
+let sumSalary=0, averageAge=0, averageTax=0;
+for(let i of employee) {
+    sumSalary += i.salary;
+    averageAge += i.age / employee.length;
+}
+
+for(let i of employee) {
+    let tax=0;
+    let netIncome = i.salary *12;
+    if(netIncome < 150000) {
+        tax = 0;
+    } else if(netIncome < 300000) {
+        tax = (netIncome - 150000)*5/100;
+    } else if(netIncome < 500000) {
+        tax = (150000)*5/100 + (netIncome - 300000)*10/100;
+    } else if(netIncome < 750000) {
+        tax = (150000)*5/100 + (200000)*10/100 + (netIncome - 500000)*15/100;
+    } else if(netIncome < 1000000) {
+        tax = (150000)*5/100 + (200000)*10/100 + (250000)*15/100 + (netIncome - 750000)*20/100;
+    } else if(netIncome < 2000000) {
+        tax = (150000)*5/100 + (200000)*10/100 + (250000)*15/100 + (250000)*20/100 + (netIncome - 1000000)*25/100;
+    } else if(netIncome < 5000000) {
+        tax = (150000)*5/100 + (200000)*10/100 + (250000)*15/100 + (250000)*20/100 + (1000000)*25/100 + (netIncome - 2000000)*30/100;
+    } else {
+        tax = (150000)*5/100 + (200000)*10/100 + (250000)*15/100 + (250000)*20/100 + (1000000)*25/100 + (3000000)*30/100 + (netIncome - 5000000)*35/100;
+    }
+    averageTax += tax;
+}
+
+console.log(sumSalary +" " +averageAge +" " +averageTax)
+
+
+
