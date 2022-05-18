b4b81d452413fdf52880106a78951f43cd8d3269
nicha.satt <nicha.satt@gmail.com>
2022-05-18 12:34:32 +0700

    addEventListener

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function alert1() {
-    alert('let me see ...');
-}
-function alert2() {
-    alert('not ok ...');
-}
 let ok = document.getElementById('ok');
-ok.addEventListener('click', alert1);
-
 let cancel = document.getElementById('cancel');
-cancel.addEventListener('click', alert2);
+let showDate = document.getElementById('showDate')
+
+ok.addEventListener('click', () => alert('let me see ...'));
+
+cancel.addEventListener('click', () => alert('not ok ...'));
+
+showDate.addEventListener('click', () => {
+    document.getElementById('emptySpace').innerText = Date();
+});
+showDate.addEventListener('dblclick', () => {
+    document.getElementById('emptySpace').innerText = "";
+});
+
+
