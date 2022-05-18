7b115707e34488641e200f78c5dd92f395732a92
nicha.satt <nicha.satt@gmail.com>
2022-05-18 12:24:24 +0700

    alert

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const newText = 'not ok';
-document.getElementById('ok').innerHTML = newText;
+function alert1() {
+    alert('let me see ...');
+}
+function alert2() {
+    alert('not ok ...');
+}
+let ok = document.getElementById('ok');
+ok.addEventListener('click', alert1);
 
-const newText2 = 'ok';
-document.getElementById('cancel').innerHTML = newText2;
+let cancel = document.getElementById('cancel');
+cancel.addEventListener('click', alert2);
